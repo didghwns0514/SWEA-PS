cb421008e8fcea84f4987d8f0264766e05189e8a
macmini <macmini@macminiui-Mac-mini.local>
2022-02-01 00:39:45 +0900

    PS(Samsung):Update;컨베이어 벨트 위의 로봇

--- 삼성/baekjoon/(34) 컨베이어 벨트 위의 로봇/run1.py
+++ 삼성/baekjoon/(34) 컨베이어 벨트 위의 로봇/run1.py
@@ -28,9 +28,9 @@ def solution(q, K, N):
 
     robotHist = []
     robotCnt = 0
-    stepCount = 1
+    stepCount = 0
 
-    while not(len([ data for data in q if data['내구도'] == 0 ]) >= K):
+    while not(len([ data for data in q if data['내구도'] <= 0 ]) >= K):
 
         # step
         stepCount += 1
@@ -54,12 +54,21 @@ def solution(q, K, N):
                 if tmpReturnValue == None: # not found
                     continue
                 else: # found
-                    nextRobotIdx = tmpReturnValue if tmpReturnValue < len(q) - 1 else 0
+                    nextRobotIdx = tmpReturnValue + 1 if tmpReturnValue < len(q) - 1 else 0
                     if q[nextRobotIdx]['내구도'] >= 1 and q[nextRobotIdx]['로봇'] == None:
                         q[nextRobotIdx]['로봇'] = copy.deepcopy(q[tmpReturnValue]['로봇'])
                         q[nextRobotIdx]['내구도'] -= 1
                         q[tmpReturnValue]['로봇'] = None
                         continue
+                    else:continue
+
+        # 로봇 내리기
+        if q[N-1]['로봇'] != None:
+            tmpRobotNum = q[N - 1]['로봇']
+            q[N - 1]['로봇'] = None
+            if tmpRobotNum in robotHist:
+                robotHist.remove(tmpRobotNum)
+            q[N - 1]['내구도'] -= 1
 
         # 로봇 올리기
         if q[0]['로봇'] == None and q[0]['내구도'] != 0:
