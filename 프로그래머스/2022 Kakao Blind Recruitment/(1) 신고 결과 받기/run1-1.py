41cc6e90e801a9cef88b340b65195df4201d8e1f
HoJun Yang <didghwns0514@gmail.com>
2022-02-13 01:00:41 +0900

    PS(2022 Kakao):신고 결과 받기 2

--- 프로그래머스/2022 Kakao Blind Recruitment/(1) 신고 결과 받기/run1-1.py
+++ 프로그래머스/2022 Kakao Blind Recruitment/(1) 신고 결과 받기/run1-1.py
@@ -16,7 +16,31 @@ class User:
 
 def solution(id_list, report, k):
 
+    data = {}
+    for _name in id_list:
+        data[_name] = {'User' : User(_name, k), 'num_reports' : 0}
 
+    for _report in report:
+        reporter, reported = _report.split(' ')
+        data[reported]['User'].addReport(reporter)
 
-    answer = []
-    return answer
+    report_count = { name : 0 for name in id_list }
+    for _name in id_list:
+        # print('-'*20)
+        # print(f'_name : {_name}')
+        if data[_name]['User'].isBanned():
+            #print(f'{_name} is banned')
+            #report_count[_name] += 1
+            #print(f"data[_name]['User'].report_set : {data[_name]['User'].report_set}")
+            for reporter in data[_name]['User'].report_set:
+                report_count[reporter] += 1
+
+    return [ report_count[_name] for _name in id_list ]
+
+if __name__ == '__main__':
+    id_list = ["muzi", "frodo", "apeach", "neo"]
+    report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+    k = 2
+
+    returnValue = solution(id_list, report, k)
+    print(f'returnValue : {returnValue}')
