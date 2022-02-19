a2656701f784e890062c3257f55829bd426b8892
HoJunYang <didghwns0514@gmail.com>
2022-02-19 16:25:18 +0900

    PS(Kakao):주차요금 계산 2

--- 프로그래머스/2022 Kakao Blind Recruitment/(3) 주차 요금 계산/run1.py
+++ 프로그래머스/2022 Kakao Blind Recruitment/(3) 주차 요금 계산/run1.py
@@ -33,10 +33,44 @@ def solution(fees, records):
                 record_data[carNum]['OUT'].append('23:59')
 
 
+    # calculate fees
+    fees_history = {
+        key:0 for key in record_data.keys()
+    }
+    for key, val in record_data.items():
+        print(f'key : {key}')
+        timeLaps = 0
+        for group in zip(val['IN'], val['OUT']):
+            print(f'group : {group}')
+            tmpTimeLaps = praseMinute(group[0], group[1])
+            print(f'tmpTimeLaps : {tmpTimeLaps}')
+            timeLaps += tmpTimeLaps
+
+        totalCharge = 0
+        if timeLaps <= 기본_시간:
+            totalCharge += 기본_요금
+        else:
+            totalCharge += 기본_요금
+            tmpUpper = ( (timeLaps - 기본_시간) / 단위_시간 )
+            tmpUpper = int(tmpUpper + 1) if int(tmpUpper) < tmpUpper else int(tmpUpper)
+            totalCharge += tmpUpper * 단위_요금
+
+        fees_history[key] = totalCharge
 
 
-    answer = []
-    return answer
+
+    return [ fees_history[key] for key in sorted(fees_history.keys()) ]
+
+def praseMinute(string1, string2):
+    h1, m1 = string1.split(':')
+    h1, m1 = int(h1), int(m1)
+
+    h2, m2 = string2.split(':')
+    h2, m2 = int(h2), int(m2)
+
+    total1, total2 = (h1 * 60) + m1, (h2 * 60) + m2
+
+    return abs(total2-total1)
 
 
 if __name__ == "__main__":
