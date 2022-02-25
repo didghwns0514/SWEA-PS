2688cbff81774e4ff76606044d8280d84f1c0c00
HoJun Yang <didghwns0514@gmail.com>
2022-02-25 21:22:02 +0900

    PS(Kakao):양궁대회 3

--- 프로그래머스/2022 Kakao Blind Recruitment/(4) 양궁대회/run1.py
+++ 프로그래머스/2022 Kakao Blind Recruitment/(4) 양궁대회/run1.py
@@ -1,6 +1,6 @@
 
 import copy
-
+import itertools
 
 curr_arrowList = []
 curr_max_point = 0
@@ -10,23 +10,45 @@ def solution(n, info):
 
     tmpArrowList = [0] * 11 # 10 -> 밖, 0 -> 정중앙 10 - index로 점수 계산
 
-    answer = []
-    return answer
+    subSolution(tmpArrowList, n)
 
 
-def subSolution(arrow_list):
+    if curr_max_point > calcScore(info):
+        return curr_arrowList
+    return [-1]
+
+
+def subSolution(arrow_list, n):
+
+    resultCombi = list(itertools.combinations_with_replacement(range(0, 11), n))
+    print(f'resultCombi : {resultCombi}')
+
+def subSolution2(arrow_list, n):
     global curr_arrowList, curr_max_point
 
-    if sum(arrow_list) == 10:
+    if sum(arrow_list) == n:
         tmpPointCalculation = calcScore(arrow_list)
         if tmpPointCalculation > curr_max_point:
             curr_max_point = tmpPointCalculation
-            curr_arrowList = arrow_list
-            return
+            curr_arrowList = copy.deepcopy(arrow_list)
         elif tmpPointCalculation == curr_max_point:
-
+            # 가장 낮음 점수를 더많이 맞춤 경우
+            for _index, prev, curr in enumerate(zip(reversed(curr_arrowList), reversed(arrow_list))):
+                if prev == curr: continue
+                elif prev > curr : break
+                else:
+                    curr_arrowList = copy.deepcopy(arrow_list)
+                    # curr_max_point = tmpPointCalculation
+                    break
         else: pass # 낮아서 업데이트 할 필요 없음
         return
+
+    for k in range(len(arrow_list)):
+        tmpArrowList = copy.deepcopy(arrow_list)
+        tmpArrowList[k] += 1
+        subSolution(tmpArrowList, n)
+
+    return
 
 def calcScore(arrow_list):
 
@@ -34,7 +56,7 @@ def calcScore(arrow_list):
 
 
 if __name__ == "__main__":
-    selection = 0
+    selection = 2
 
     if selection == 0:
         n = 5
@@ -48,5 +70,6 @@ if __name__ == "__main__":
         n = 9
         info = 	[0,0,1,2,0,1,1,1,1,1,1]
 
-    solution(n, info)
+    returnValue = solution(n, info)
+    print(f'returnValue : {returnValue}')
 
