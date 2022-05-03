aeec9413e3ae01d062b15db1abecbe509a820489
HoJun Yang <didghwns0514@gmail.com>
2022-05-04 07:00:22 +0900

    PS(2022 Kakao): 파괴되지 않는 건물 1

---
+++ 프로그래머스/2022 Kakao Blind Recruitment/(6) 파괴되지 않는 건물/run1.py
@@ -0,0 +1,26 @@
+
+def solution(board, skills):
+
+    for skill in skills:
+        pass
+
+    answer = 0
+    return answer
+
+if __name__ == "__main__":
+
+    switch = 2
+
+    if switch == 1:
+        board = [[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]]
+        skill = [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]]
+        answer = solution(board, skill)
+        print(f'found answer : {answer}')
+        assert(answer == 10)
+
+    elif switch == 2:
+        board = [[1,2,3],[4,5,6],[7,8,9]]
+        skill = [[1,1,1,2,2,4],[1,0,0,1,1,2],[2,2,0,2,0,100]]
+        answer = solution(board, skill)
+        print(f'found answer : {answer}')
+        assert(answer == 6)
